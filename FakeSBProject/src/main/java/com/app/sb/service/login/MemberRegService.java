c5c040f810abd75473e4b8667c4eade51fe0c6b3
jennakim1007 <jennakim1007@gmail.com>
2022-11-30 18:01:55 +0900

    MemberRegister 기능 추가 수정중

--- FakeSBProject/src/main/java/com/app/sb/service/login/MemberRegService.java
+++ FakeSBProject/src/main/java/com/app/sb/service/login/MemberRegService.java
@@ -1,7 +1,21 @@
 package com.app.sb.service.login;
 
+import com.app.sb.domain.MemberDTO;
+import com.app.sb.domain.MemberRegRequestDTO;
+import com.app.sb.mapper.MemberMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberRegService {
+
+    @Autowired
+    public MemberMapper memberMapper;
+
+    public int RegisterMember(MemberRegRequestDTO memberRegRequestDTO){
+        MemberDTO memberDTO = memberRegRequestDTO.toMember();
+        return memberMapper.insertMemer(memberDTO);
+    }
+
+
 }

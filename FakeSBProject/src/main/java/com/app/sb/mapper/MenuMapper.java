27aca727d617e540b4277488c87fe3cff886c939
dabinHam <113006964+dabinHam@users.noreply.github.com>
2022-12-01 18:43:41 +0900

    mapper 수정
    search type 추가관련 수정

--- FakeSBProject/src/main/java/com/app/sb/mapper/MenuMapper.java
+++ FakeSBProject/src/main/java/com/app/sb/mapper/MenuMapper.java
@@ -2,6 +2,7 @@ package com.app.sb.mapper;
 
 import com.app.sb.domain.MemberDTO;
 import com.app.sb.domain.MenuDTO;
+import com.app.sb.domain.MenuThemeOption;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -12,6 +13,9 @@ public interface MenuMapper {
 
     // 메뉴 전체 출력
     List<MenuDTO> selectAll();
+
+    List<MenuDTO> selectByOption(MenuThemeOption themeOption);
+    List<MenuDTO> selectByMdxs(List<Integer> mdxs);
 
     // 상세페이지 , mdx 값으로 하나의 product 정보를 가져오는 메소드
     MenuDTO selectByMdx(long mdx);

918c83d02c3aa236405e45b68fad11afccb9643e
jennakim1007 <jennakim1007@gmail.com>
2022-12-01 19:39:47 +0900

    회원탈퇴 기능 구현중

--- FakeSBProject/src/main/java/com/app/sb/controller/member/MemberModifyController.java
+++ FakeSBProject/src/main/java/com/app/sb/controller/member/MemberModifyController.java
@@ -6,6 +6,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- FakeSBProject/src/main/java/com/app/sb/controller/member/MemberRemoveController.java
+++ FakeSBProject/src/main/java/com/app/sb/controller/member/MemberRemoveController.java
@@ -5,8 +5,7 @@ import com.app.sb.service.login.MemberRemoveService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @Log4j2
@@ -15,12 +14,18 @@ public class MemberRemoveController {
     @Autowired
     private MemberRemoveService memberRemoveService;
 
-    @GetMapping("/member/delete")
-    public String deleteMember(@RequestParam("uid") String uid){
-        log.info("delete..." + uid);
-        memberRemoveService.deleteMember(uid);
-        return "redirect:index";
+    @RequestMapping(value = "/member/delete", method = RequestMethod.GET)
+    public String getDeletePage(){
+      return "/member/deleteForm";
     }
 
+    @RequestMapping(value = "/member/delete/{uid}", method = RequestMethod.POST)
+    public String deleteMember(@PathVariable("uid") String uid) {
+        log.info("delete..." + uid);
+        String path = "/delete/member/uid?=uid";
+        memberRemoveService.deleteMember(uid);
+        return "/index";
+
+    }
 
 }

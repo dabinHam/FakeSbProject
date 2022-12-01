7c9a89521aafbbd3a7dc8cc4c8d040f84e335a3b
jennakim1007 <jennakim1007@gmail.com>
2022-12-01 10:28:28 +0900

    Test - MemberInsert 확인완료

--- FakeSBProject/src/test/java/MemberTest.java
+++ FakeSBProject/src/test/java/MemberTest.java
@@ -1,3 +1,6 @@
+import com.app.sb.domain.MemberDTO;
+import com.app.sb.domain.MemberRegRequestDTO;
+import com.app.sb.mapper.MemberMapper;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -15,6 +18,17 @@ import java.sql.SQLException;
 @ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/spring/root-context.xml")
 public class MemberTest {
 
+    @Autowired
+    private MemberMapper memberMapper;
+
+    @Test
+    public void insertMemerTest(){
+
+        MemberDTO memberDTO = MemberDTO.builder().uid("a").upw("a").uname("a").nickname("a").phone("a").email("a").address1("a").address2("a").uphoto("a").build();
+        log.info(memberDTO);
+        int result = memberMapper.insertMemer(memberDTO);
+        Assertions.assertEquals(1, result);
+    }
 
 
 }

92505acef58449220961fbaf513df8ac3fb70a09
dabinHam <113006964+dabinHam@users.noreply.github.com>
2022-12-01 10:42:39 +0900

    list page 연결

--- FakeSBProject/src/main/java/com/app/sb/controller/menu/MenuListController.java
+++ FakeSBProject/src/main/java/com/app/sb/controller/menu/MenuListController.java
@@ -4,6 +4,7 @@ import com.app.sb.service.menu.MenuListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,10 +14,10 @@ public class MenuListController {
     @Autowired
     private MenuListService menuListService;
 
-    @RequestMapping("/menu/list")
+    @GetMapping
     public String getMenuList(Model model){
-        model.addAttribute("menuList",menuListService.getMenuList());
-        return "list";
+//        model.addAttribute("menuList",menuListService.getMenuList());
+        return "menu/list";
     }
 
 

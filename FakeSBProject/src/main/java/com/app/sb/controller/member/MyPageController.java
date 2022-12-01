1dc718c8143bc9296ebacea88210d0f4edafdabb
jennakim1007 <jennakim1007@gmail.com>
2022-12-01 11:15:07 +0900

    회원가입, 마이페이지 view 작성

--- FakeSBProject/src/main/java/com/app/sb/controller/member/MyPageController.java
+++ FakeSBProject/src/main/java/com/app/sb/controller/member/MyPageController.java
@@ -1,18 +1,17 @@
 package com.app.sb.controller.member;
 
-import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Log4j2
 @Controller
-@RequestMapping("/login")
-public class LoginController {
+@RequestMapping("/member/mystarbucks")
+public class MyPageController {
 
     @GetMapping
-    public String getLoginPage(){
-        return "member/loginForm";
+    public String getMyPage(){
+        return "member/myStarbucksForm";
     }
+
 
 }
